2b7f106946c74d09bdc2b88ff3cee6e89b64c4cd
bitbytebytes <bytebit@bitbytebytes.io>
2026-04-25 07:38:51 -0400

    added map data validation and logging

--- Loader.gd
+++ Loader.gd
@@ -28,12 +28,58 @@ func LoadScene(scene : String):
 
 
 func add_shelter(shelter: Dictionary) -> bool:
-    added_shelters[shelter.map_name] = shelter
-    print("[B_Loader] shelter added: " + shelter.map_name)
-    return true
+    if _validate(shelter):
+        added_shelters[shelter.map_name] = shelter
+        _log("Shelter added: " + shelter.map_name)
+        return true
+    else:
+        _log("Failed to add " + shelter.map_name)
+        return false
 
 
 func add_map(map: Dictionary) -> bool:
-    added_maps[map.map_name] = map
-    print("[B_Loader] map added: " +  map.map_name)
-    return true
+    if _validate(map):
+        added_maps[map.map_name] = map
+        _log("Map added: " +  map.map_name)
+        return true
+    else:
+        _log("Failed to add map " + map.mapName)
+        return false
+
+
+func _validate(data: Dictionary) -> bool:
+    _log("Validating " + data.map_name)
+    var data_valid = true
+    if not data.map_name or data.map_name == "":
+        _log("Map name not set")
+        data_valid = false
+    if data.map_name in added_shelters or data.map_name in added_maps:
+        _log("Map with name " + data.map_name + " already added")
+        data_valid = false
+    if not FileAccess.file_exists(data.scene_path):
+        _log("Failed to locate " + data.scene_path)
+        data_valid = false
+    if not data.entrance_spawn or data.entrance_spawn == "":
+        _log("No entrance spawm set")
+        data_valid = false
+    if not data.exit_spawn or data.exit_spawn == "":
+        _log("No exit spawm set")
+        data_valid = false
+    if not data.connected_to or data.connected_to == "":
+        _log("No map connection set")
+        data_valid = false
+    for item in data.connected_content:
+        if not FileAccess.file_exists(item.path):
+            _log("Failed to locate " + data.scene_path)
+            data_valid = false
+        if not item.position is Vector3:
+            _log("Invalid position for " + item.path)
+            data_valid = false
+        if not item.rotation is Vector3:
+            _log("Invalid rotation for " + item.path)
+            data_valid = false
+    return data_valid
+ 
+
+func _log(message: String) -> void:
+    print("[B_Loader] " + message)
